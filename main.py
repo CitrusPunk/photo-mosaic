54c15ea02abbff30bdf4916f714e372513db885a
Marian K <marian.kloesler@gmx.de>
2023-05-31 18:35:24 +0200

    added generator for test images and restructured main.py

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ import ColorAnalyzer as ca
 preview = True
 
 file_extensions = ["jpg", "jpeg", "png"]
-is_caching_tiles = False
+is_caching_tiles = True
 tile_cache_name = "tile_cache.json"
 tile_path = "tiles_images"
 
 is_caching_source = True
 source_cache_name = "source_cache.json"
-source_path = "source.jpg"
+source_path = "source06.jpg"
 
-input_tile_size = 100
-output_tile_size = 10
+input_tile_size = 32
+output_tile_size = 196
 in_out_ratio = output_tile_size / input_tile_size
 
 
@@ -103,53 +103,80 @@ def cache_tiles(origin="tiles_images/", cache_name="cache.json"):
     print("\nCached images in " + origin)
 
 
-print("=== Photomosaic ===")
-start_time = time.time()
+def get_partial_img(data):
+    core, x0, y0, source_averages, data_tiles = data
+    print(core, x0, y0)
+    tilized_img = np.zeros(
+        (y0*output_tile_size, x0*output_tile_size, 3), np.uint8)
+    print(tilized_img.shape)
+    for y in range(y0, core*y0):
+        for x in range(x0, core*x0):
+            src_tuple = eval(source_averages[x + y * core*x0])
+            closest_color = ca.get_closest_color(src_tuple, data_tiles.keys())
 
-if is_caching_tiles or tile_cache_name not in os.listdir():
-    print("Started tile caching process...")
-    try:
-        cache_tiles(tile_path, tile_cache_name)
-    except Exception as e:
-        print("Tile caching failed with error:\n", e)
+            tile_path = random.choice(data_tiles[str(closest_color)])
+            tile_image = cv2.imread(tile_path)
+            tile_image = resize_with_ratio(tile_image, output_tile_size)
+            tile_image = tilize(tile_image, output_tile_size)
 
-if is_caching_source or source_cache_name not in os.listdir():
-    print("Started source caching process...")
-    try:
-        cache_image(input_tile_size, source_path, source_cache_name)
-    except Exception as e:
-        print("Source caching failed with error:\n", e)
+            u0, u1, v0, v1 = get_tile_coordinates(x, y, output_tile_size)
+            # print(tile_image)
+            tilized_img[v0:v1, u0:u1] = tile_image
+    print('core ', core, ' done.')
+    cv2.imwrite("core_" + str(core) + ".jpg", tilized_img)
 
-# load cached files
-with open(tile_cache_name, "r") as file:
-    data_tiles = json.load(file)
-with open(source_cache_name, "r") as file:
-    data_source = json.load(file)
-num_tiles_h, num_tiles_w = data_source[:2]
-source_averages = data_source[2:]
 
-# get tile array
-tiles = []
-tilized_img = np.zeros((output_tile_size * num_tiles_h,
-                       output_tile_size * num_tiles_w, 3), np.uint8)
-for y in range(0, num_tiles_h):
-    for x in range(0, num_tiles_w):
-        source_tuple = eval(source_averages[x + y * num_tiles_w])
-        closest_color = ca.get_closest_color(source_tuple, data_tiles.keys())
+if __name__ == '__main__':
+    print("=== Photomosaic ===")
+    start_time = time.time()
 
-        tile_path = random.choice(data_tiles[str(closest_color)])
-        tile_image = cv2.imread(tile_path)
-        tile_image = resize_with_ratio(tile_image, output_tile_size)
-        tile_image = tilize(tile_image, output_tile_size)
+    if is_caching_tiles or tile_cache_name not in os.listdir():
+        print("Started tile caching process...")
+        try:
+            cache_tiles(tile_path, tile_cache_name)
+        except Exception as e:
+            print("Tile caching failed with error:\n", e)
+            print("Path: ", tile_path)
 
-        u0, u1, v0, v1 = get_tile_coordinates(x, y, output_tile_size)
-        tilized_img[v0:v1, u0:u1] = tile_image
+    if is_caching_source or source_cache_name not in os.listdir():
+        print("Started source caching process...")
+        try:
+            cache_image(input_tile_size, source_path, source_cache_name)
+        except Exception as e:
+            print("Source caching failed with error:\n", e)
 
-        print("\r", x + y * num_tiles_w, " of ",
-              num_tiles_h * num_tiles_w, " tiles done.", end='')
+    # load cached files
+    with open(tile_cache_name, "r") as file:
+        data_tiles = json.load(file)
+    with open(source_cache_name, "r") as file:
+        data_source = json.load(file)
+    num_tiles_h, num_tiles_w = data_source[:2]
+    source_averages = data_source[2:]
 
-cv2.imwrite("output.jpg", tilized_img)
+    # get tile array
+    tiles = []
+    tilized_img = np.zeros((output_tile_size * num_tiles_h,
+                           output_tile_size * num_tiles_w, 3), np.uint8)
 
-end_time = time.time()
-total_time = end_time - start_time
-print("\n\nPhotomosaic generated in ", round(total_time, 2), " seconds.")
+    for y in range(0, num_tiles_h):
+        for x in range(0, num_tiles_w):
+            source_tuple = eval(source_averages[x + y * num_tiles_w])
+            closest_color = ca.get_closest_color(
+                source_tuple, data_tiles.keys())
+
+            tile_path = random.choice(data_tiles[str(closest_color)])
+            tile_image = cv2.imread(tile_path)
+            tile_image = resize_with_ratio(tile_image, output_tile_size)
+            tile_image = tilize(tile_image, output_tile_size)
+
+            u0, u1, v0, v1 = get_tile_coordinates(x, y, output_tile_size)
+            tilized_img[v0:v1, u0:u1] = tile_image
+
+            print("\r", x + y * num_tiles_w, " of ",
+                  num_tiles_h * num_tiles_w, " tiles done.", end='')
+
+    cv2.imwrite("output.jpg", tilized_img)
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    print("\n\nPhotomosaic generated in ", round(total_time, 2), " seconds.")
